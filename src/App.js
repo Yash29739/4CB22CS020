3e24ae27b8b2b29d9a532cde780967f552590a78
B_M_Yashwanth <bhagyapushpagiri17@gmail.com>
2025-04-11 22:02:34 +0530

    updates: minor design changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import Navbar from './Navbar';
 import { BrowserRouter as  Router,Routes,Route } from 'react-router-dom';
 import TopUsers from './TopUsers';
 import TrendingPosts from './TrendingPosts';
 import Feeds from './Feeds';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='topUsers' element={<TopUsers/>}/>
-        <Route path='feeds' element={<Feeds/>}/>
-        <Route path='trendingPosts' element={<TrendingPosts/>}/>
+        <Route path='/' element={<Feeds/>}/>
+        <Route path='/topUsers' element={<TopUsers/>}/>
+        <Route path='/trendingPosts' element={<TrendingPosts/>}/>
       </Routes>
     </Router>
   );
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
-    <h1 className="text-2xl font-bold text-yellow-500">Socio<span className='text-2xl font-bold text-yellow-700'>Tech</span></h1>
+    <nav className="bg-blue-50 px-6 py-4 flex justify-between items-center">
+    <h1 className="text-2xl font-bold text-yellow-400">Socio<span className='text-2xl font-bold text-blue-700'>Tech</span></h1>
 
     <ul className="flex gap-8 text-gray-700 font-medium text-lg">
       <li>
@@ -14,10 +14,11 @@ const Navbar = () => {
         <Link to="/trendingPosts" className="hover:text-blue-500">Trending Posts</Link>
       </li>
       <li>
-        <Link to="/feeds" className="hover:text-blue-500">Feed</Link>
+        <Link to="/" className="hover:text-blue-500">Home</Link>
       </li>
     </ul>
   </nav>
+  
   )
 }
 
